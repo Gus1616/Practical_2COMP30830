343b26c0e19bf5fae2d92e476ad480b3c505ba11
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-01-27 12:31:50 +0000

    fixed setup

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="practical_two",
     version="1.0.0",
     description='Practical_2',
-    packages=["Practical_2COMP30830"],
+    packages=["systeminfo"],
     entry_points={
         'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
-    zip_safe=False)
+        }
+    )
