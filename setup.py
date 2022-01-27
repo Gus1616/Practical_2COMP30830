6333df802eec11aaa8cde55782c26212019e7975
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-01-27 14:51:43 +0000

    fixes

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version="0.1",
     description='Practical_2',
     url='https://github.com/Gus1616/Practical_2COMP30830',
-    packages=["systeminfo"],
-    scripts=['bin/comp303830_systeminfo'],
-    # entry_points={
-    #     'console_scripts':['comp30380_systeminfo=systeminfo.main:main']
-    #     }
+    packages=["systeminfo_example"],
+    # scripts=['bin/comp303830_systeminfo'],
+    entry_points={
+        'console_scripts':['comp30380_systeminfo=systeminfo_example.command_line:main']
+        }
     )
