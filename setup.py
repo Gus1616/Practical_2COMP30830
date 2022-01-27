2871f7f2c6524e6fbb38b844383e57d9b8e83284
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-01-27 14:16:24 +0000

    fix init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="practical_two",
-    version="1.0.0",
+    version="1.1.0",
     description='Practical_2',
     url='https://github.com/Gus1616/Practical_2COMP30830',
     packages=["systeminfo"],
