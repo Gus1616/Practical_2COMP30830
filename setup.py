10057bd67f583a7f19f745b8ebd5f15b40d3cf6e
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-01-27 12:05:33 +0000

    fixed file structure

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name="practical_two",
     version="1.0.0",
-    description='Practical 2',
-    scripts=["main.py"],
-)
+    description='Practical_2',
+    packages=["Practical_2COMP30830"],
+    entry_points={
+        'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+    zip_safe=False)
