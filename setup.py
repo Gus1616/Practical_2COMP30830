abe76e16e9777ba3f6ae0d5cc343c47dfa6db937
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-01-27 12:46:01 +0000

    adding scripts

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0.0",
     description='Practical_2',
     packages=["systeminfo"],
+    scripts=['bin/comp30670'],
     entry_points={
         'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
         }
